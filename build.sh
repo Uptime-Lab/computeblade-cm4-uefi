1aeba6e2f42375a39a793a0e0b8caa704ec9422f
Pete Batard <pete@akeo.ie>
2024-04-11 08:23:35 +0200

    Embed the additional 2023 Secure Boot certificates from Microsoft
    Signed-off-by: Matthias Riegler <matthias.riegler@ankorstore.com>

--- build.sh
+++ build.sh
@@ -7,9 +7,13 @@ set -e
 # Setup default secure boot keys
 mkdir -p keys
 openssl req -new -x509 -newkey rsa:2048 -subj "/CN=Raspberry Pi Platform Key/" -keyout /dev/null -outform DER -out keys/pk.cer -days 7300 -nodes -sha256
+curl -L https://go.microsoft.com/fwlink/?LinkId=321185 -o keys/ms_kek1.cer
+curl -L https://go.microsoft.com/fwlink/?linkid=2239775 -o keys/ms_kek2.cer
 curl -L https://go.microsoft.com/fwlink/?LinkId=321185 -o keys/ms_kek.cer
 curl -L https://go.microsoft.com/fwlink/?linkid=321192 -o keys/ms_db1.cer
 curl -L https://go.microsoft.com/fwlink/?linkid=321194 -o keys/ms_db2.cer
+curl -L https://go.microsoft.com/fwlink/?linkid=2239776 -o keys/ms_db3.cer
+curl -L https://go.microsoft.com/fwlink/?linkid=2239872 -o keys/ms_db4.cer
 curl -L https://uefi.org/sites/default/files/resources/dbxupdate_arm64.bin -o keys/arm64_dbx.bin
 
 # Apply patches
@@ -17,7 +21,7 @@ curl -L https://uefi.org/sites/default/files/resources/dbxupdate_arm64.bin -o ke
 
 BUILD_FLAGS="-D SECURE_BOOT_ENABLE=TRUE -D INCLUDE_TFTP_COMMAND=TRUE -D NETWORK_ISCSI_ENABLE=TRUE -D SMC_PCI_SUPPORT=1"
 
-DEFAULT_KEYS="-D DEFAULT_KEYS=TRUE -D PK_DEFAULT_FILE=$WORKSPACE/keys/pk.cer -D KEK_DEFAULT_FILE1=$WORKSPACE/keys/ms_kek.cer -D DB_DEFAULT_FILE1=$WORKSPACE/keys/ms_db1.cer -D DB_DEFAULT_FILE2=$WORKSPACE/keys/ms_db2.cer -D DBX_DEFAULT_FILE1=$WORKSPACE/keys/arm64_dbx.bin"
+DEFAULT_KEYS="-D DEFAULT_KEYS=TRUE -D PK_DEFAULT_FILE=$WORKSPACE/keys/pk.cer -D KEK_DEFAULT_FILE1=$WORKSPACE/keys/ms_kek1.cer -D KEK_DEFAULT_FILE2=$WORKSPACE/keys/ms_kek2.cer -D DB_DEFAULT_FILE1=$WORKSPACE/keys/ms_db1.cer -D DB_DEFAULT_FILE2=$WORKSPACE/keys/ms_db2.cer -D DB_DEFAULT_FILE3=$WORKSPACE/keys/ms_db3.cer -D DB_DEFAULT_FILE4=$WORKSPACE/keys/ms_db4.cer -D DBX_DEFAULT_FILE1=$WORKSPACE/keys/arm64_dbx.bin"
 
 # EDK2's 'build' command doesn't play nice with spaces in environmnent variables, so we can't move the PCDs there...
 source ./edk2/edksetup.sh
